09aa461956944774f247e4e47afd4620920c93f6
Azizbeko9o <umidovicho9o@gmail.com>
2025-04-17 19:23:52 +0500

    Add files via upload

---
+++ lesson-6/homework/lesson-6.py
@@ -0,0 +1,10 @@
+# def simple_generator():
+#     yield 1
+#     yield 2
+#     yield 3
+# val = simple_generator
+# print(val)
+# def say_hello():
+#     print("Hello!")
+    
+# print(say_hello())
